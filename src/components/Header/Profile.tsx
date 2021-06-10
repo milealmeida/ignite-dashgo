1ea385ddb7bde14b10015dfd045aa7828b9e690d
Milena <milenaalmeidadev@hotmail.com>
2021-06-10 19:37:37 -0400

    Responsividade

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,14 +1,20 @@
 import { Avatar, Box, Flex, Text } from '@chakra-ui/react';
 
-export function Profile() {
+interface ProfileProps {
+    showProfileData?: boolean;  
+}
+
+export function Profile({ showProfileData = true }: ProfileProps) {
     return(
         <Flex align="center">
-            <Box mr="4" textAlign="right">
-                <Text>Milena Almeida</Text>
-                <Text color="gray.300" fontSize="small">
-                    milenaalmeidadev@hotmail.com
-                </Text>
-            </Box>
+            { showProfileData && (
+                <Box mr="4" textAlign="right">
+                    <Text>Milena Almeida</Text>
+                    <Text color="gray.300" fontSize="small">
+                        milenaalmeidadev@hotmail.com
+                    </Text>
+                </Box>
+            )}
 
             <Avatar size="md" name="Milena Almeida" src="https://github.com/milealmeida.png" />
         </Flex>
